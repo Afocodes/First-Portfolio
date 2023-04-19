5861cadebb6d1458db0806d4886845211870a557
Afonso M <afonsojmpt@gmail.com>
2023-04-19 19:18:18 +0100

    Text changed on the footer

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,5 +5,5 @@ var startTime = new Date().getTime();
 setInterval(function () {
     var currentTime = new Date().getTime();
     var timeSpent = (currentTime - startTime) / 1000; // Convert to seconds
-    document.getElementById("time-spent").innerHTML = "Time spent on this page: " + timeSpent + " seconds";
+    document.getElementById("time-spent").innerHTML = "You spent so far " + timeSpent + " seconds without connecting with me on LinkedIn";
 }, 1000);
